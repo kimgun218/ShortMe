f0436b4f278e02f7f5fab47efd1bf36fd3bbd440
K!MGVN <kimgun218@gmail.com>
2017-09-19 01:05:24 +0700

    Initial project with pom.xml and main class

---
+++ src/main/java/com/kimgun/shortme/ShortMeMain.java
@@ -0,0 +1,12 @@
+package com.kimgun.shortme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortMeMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortMeMain.class, args);
+    }
+}
